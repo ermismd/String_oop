815d22b0e4d7c0212527bec36dc8e0c0c5905005
ermismd <ermismdelopoulos@outlook.com>
2024-06-01 00:25:47 +0200

    Data abstraction, class String to get a character at index from an array

---
+++ strings-oop/src/String.java
@@ -0,0 +1,28 @@
+
+
+
+/**
+ * Each instance of this class represents a sequence of text characters
+ * @immutable
+ * 
+ * @invar|getLength()==getCharacters().length
+ * 
+ */
+public class String {
+	
+	
+	
+	public char[] getCharacters() {
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	
+	public int getLength() {
+		throw new RuntimeException("Not yet impl");
+	}
+	
+	public char getCharacterAt(int index) {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+}
