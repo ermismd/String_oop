b4fb907edd94c29dfe46104cdb56679e7ac14986
ermismd <ermismdelopoulos@outlook.com>
2024-06-01 18:18:38 -0500

    minor add ons to formal documentation

--- strings-oop/src/String.java
+++ strings-oop/src/String.java
@@ -1,28 +1,68 @@
 
-
+import java.util.Arrays;
 
 /**
  * Each instance of this class represents a sequence of text characters
- * @immutable
  * 
  * @invar|getLength()==getCharacters().length
+ * @invar|getCharacters()!=null
+ * 
+ * @immutable
  * 
  */
 public class String {
 	
 	
-	
+	/**
+	 * 
+	 * @inspects|this 
+	 * @Creates | result
+	 * 
+	 */
 	public char[] getCharacters() {
 		throw new RuntimeException("Not yet implemented");
 	}
 
-	
+	/**
+	 * 
+	 * @inspects|this
+	 * 
+	 */
 	public int getLength() {
 		throw new RuntimeException("Not yet impl");
 	}
 	
+	/**
+	 * @pre|index>0&&index<= getLength()
+	 * 
+	 * @inspects|this
+	 * 
+	 * @post|result==getCharacters()[index]
+	 * 
+	 */
 	public char getCharacterAt(int index) {
 		throw new RuntimeException("Not yet implemented");
 	}
+	
+	
+	
+	/**	 
+	 * @pre|characters!=null
+	 *  
+	 * @inspects|characters
+	 * 
+	 * @post|Arrays.equals(getCharacters(),characters)
+	 */
+	public String(char[] characters) {
+		throw new RuntimeException("not yet impl");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
